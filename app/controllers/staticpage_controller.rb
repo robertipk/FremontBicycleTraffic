e0e95ba3914667fae3e90aa3c658ea52fb1f0ef4
Rob <robertipk@gmail.com>
2015-12-25 14:02:47 -0500

    Added more controller code, added picture of original Socrata dataset and more explanations

--- app/controllers/staticpage_controller.rb
+++ app/controllers/staticpage_controller.rb
@@ -9,10 +9,10 @@ class StaticpageController < ApplicationController
   def rawjson
 	client = SODA::Client.new({:domain => "data.seattle.gov/",
                            :app_token => "AU94c3BhpwNnRY8ExL34d2W4x"})
-	#responsse = client.get("4xy5-26gy", {"$where" => "date > '2014-01-01T12:00:00' AND date < '2015-01-02T18:00:00'"})
-	respe = client.get("4xy5-26gy", {"$order" => "'ASC'"})
+	response = client.get("4xy5-26gy", {"$where" => "date > '2014-01-01T12:00:00' AND date < '2015-01-02T18:00:00'"})
+	#response = client.get("4xy5-26gy", {"$order" => "'ASC'"})
 
-	respe.each do |r|
+	response.each do |r|
 		record = Tally.find_or_create_by(date: r.date, fremont_bridge_nb: r.fremont_bridge_nb, fremont_bridge_sb: r.fremont_bridge_sb)
 		record.save
 	end
