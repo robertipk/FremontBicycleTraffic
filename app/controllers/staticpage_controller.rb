0792d2ef7b8b26b45d984602924e0f9dadd2cfca
Rob <robertipk@gmail.com>
2015-12-25 15:03:54 -0500

    Added screenshots of data in organized table and explanations

--- app/controllers/staticpage_controller.rb
+++ app/controllers/staticpage_controller.rb
@@ -9,8 +9,8 @@ class StaticpageController < ApplicationController
   def rawjson
 	client = SODA::Client.new({:domain => "data.seattle.gov/",
                            :app_token => "AU94c3BhpwNnRY8ExL34d2W4x"})
-	response = client.get("4xy5-26gy", {"$where" => "date > '2014-01-01T12:00:00' AND date < '2015-01-02T18:00:00'"})
-	#response = client.get("4xy5-26gy", {"$order" => "'ASC'"})
+	#response = client.get("4xy5-26gy", {"$where" => "date > '2014-01-01T12:00:00' AND date < '2015-01-02T18:00:00'"})
+	response = client.get("4xy5-26gy", {"$order" => "'ASC'"})
 
 	response.each do |r|
 		record = Tally.find_or_create_by(date: r.date, fremont_bridge_nb: r.fremont_bridge_nb, fremont_bridge_sb: r.fremont_bridge_sb)
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( test.js )
 Rails.application.config.assets.precompile += %w( databasepic.png )
+Rails.application.config.assets.precompile += %w( sublimecode.png )
