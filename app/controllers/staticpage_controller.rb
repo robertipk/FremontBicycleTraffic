2d34c65b9ce6ab61f64148a95a864b0ac25c0a59
Rob <robertipk@gmail.com>
2015-12-28 02:08:23 -0500

    Added timeline, jquery toggle sliding effects

--- app/controllers/staticpage_controller.rb
+++ app/controllers/staticpage_controller.rb
@@ -1,25 +1,6 @@
 require 'soda/client'
 require 'pp'
 class StaticpageController < ApplicationController
-  def index
-    Count.delete_all
-    client = SODA::Client.new({:domain => "data.seattle.gov/",
-                             :app_token => "AU94c3BhpwNnRY8ExL34d2W4x"})
-    
-    response = client.get("4xy5-26gy", {"$where" => "date > '2014-04-01T12:00:00' AND date < '2014-06-02T18:00:00'"})
-
-
-    number = 1
-    response.each do |r|
-      record = Count.create(date: r.date, fremont_bridge_nb: r.fremont_bridge_nb, fremont_bridge_sb: r.fremont_bridge_sb)
-      record.id = number
-      number = number + 1
-      record.save
-    end
-
-
-    @data = Count.limit(1000).order(date: :asc)
-  end
 
   def visual
   end
@@ -32,44 +13,48 @@ class StaticpageController < ApplicationController
 
   def newgraph
   end
-
-  def parse_form
+  
+  def index
     Count.delete_all
-    @timepoint1 = Query.new
-    @timepoint2 = Query.new
-    #@timepoint2 = Query.new
-    #@timepoint2.timepoint = params[:t2].to_s
-    @timepoint1.timepoint = params[:t1]
-    @timepoint2.timepoint = params[:t2]
-
-
     client = SODA::Client.new({:domain => "data.seattle.gov/",
                              :app_token => "AU94c3BhpwNnRY8ExL34d2W4x"})
-    firstdate = @timepoint1.timepoint
-    seconddate = @timepoint2.timepoint
-
-    dates = "date > '#{firstdate}' AND date < '#{seconddate}'"
-
-    #dates = "date < '#{firstdate}'"
-    responsee = client.get("4xy5-26gy", "$where" => dates)
-
+    response = client.get("4xy5-26gy", {"$where" => "date > '2014-04-01T12:00:00' AND date < '2014-06-02T18:00:00'"})
     number = 1
-    responsee.each do |r|
+    response.each do |r|
       record = Count.create(date: r.date, fremont_bridge_nb: r.fremont_bridge_nb, fremont_bridge_sb: r.fremont_bridge_sb)
       record.id = number
       number = number + 1
       record.save
     end
+    @data = Count.limit(1000).order(date: :asc)
+  end
 
+  def parse_form
+    Count.delete_all
+    @timepoint1 = Query.new
+    @timepoint2 = Query.new
+    @timepoint1.timepoint = params[:t1]
+    @timepoint2.timepoint = params[:t2]
 
+    client = SODA::Client.new({:domain => "data.seattle.gov/",
+                             :app_token => "AU94c3BhpwNnRY8ExL34d2W4x"})
+    firstdate = @timepoint1.timepoint
+    seconddate = @timepoint2.timepoint
+    dates = "date > '#{firstdate}' AND date < '#{seconddate}'"
+    callback = client.get("4xy5-26gy", "$where" => dates)
+    number = 1
+    callback.each do |r|
+      record = Count.create(date: r.date, fremont_bridge_nb: r.fremont_bridge_nb, fremont_bridge_sb: r.fremont_bridge_sb)
+      record.id = number
+      number = number + 1
+      record.save
+    end
     @new = Count.limit(950).order(date: :asc)
     render :newgraph
-
   end
 
   def rawjson
     Count.delete_all
-
   	client = SODA::Client.new({:domain => "data.seattle.gov/",
                              :app_token => "AU94c3BhpwNnRY8ExL34d2W4x"})
   	response = client.get("4xy5-26gy", {"$where" => "date > '2014-04-01T12:00:00' AND date < '2014-06-02T18:00:00'"})
@@ -84,7 +69,5 @@ class StaticpageController < ApplicationController
 
     @data = Count.limit(500).order(date: :asc)
   end
-
-
 
 end
